284f8d26e66fdf1ce0632c84a5207b89c954f3f4
Denis Dvornikov <candyboobers@gmail.com>
2026-03-15 19:35:06 +0100

    get back diagnostic to status column

--- .config/nvim/lua/pkg/statuscolumn.lua
+++ .config/nvim/lua/pkg/statuscolumn.lua
@@ -15,12 +15,14 @@ local function ns_name(id)
 end
 
 ---@param lnum integer
----@return [vim.api.keyset.extmark_details, vim.api.keyset.extmark_details]
+---@return [vim.api.keyset.extmark_details, vim.api.keyset.extmark_details, vim.api.keyset.extmark_details]
 local function get_signs(lnum)
   local signs = api.nvim_buf_get_extmarks(0, -1, { lnum - 1, 0 }, { lnum - 1, -1 }, { details = true, type = "sign" })
 
   ---@type vim.api.keyset.extmark_details?
   local dap
+  ---@type vim.api.keyset.extmark_details?
+  local diag
   ---@type vim.api.keyset.extmark_details?
   local git
   for _, mark in ipairs(signs) do
@@ -30,6 +32,13 @@ local function get_signs(lnum)
         if not dap or details.priority > dap.priority then
           dap = details
         end
+      elseif
+        (details.sign_name and details.sign_name:match("^DiagnosticSign"))
+        or ns_name(details.ns_id):find("diagnostic")
+      then
+        if not diag or details.priority > diag.priority then
+          diag = details
+        end
       elseif ns_name(details.ns_id):find("gitsigns") then
         if not git or details.priority > git.priority then
           git = details
@@ -38,7 +47,7 @@ local function get_signs(lnum)
     end
   end
 
-  return { dap, git }
+  return { dap, diag, git }
 end
 
 ---@param sign vim.api.keyset.extmark_details?
@@ -49,6 +58,12 @@ end
 ---@param sign vim.api.keyset.extmark_details?
 local function render_git(sign)
   return sign and "%#" .. sign.sign_hl_group .. "#" .. sign.sign_text .. "%*" or "  "
+end
+
+---@param sign vim.api.keyset.extmark_details?
+local function render_diag(sign)
+  local hl = sign and (sign.sign_hl_group or sign.sign_name)
+  return sign and hl and "%#" .. hl .. "#" .. sign.sign_text .. "%*" or "  "
 end
 
 local function number_line()
@@ -70,9 +85,12 @@ M.render = function()
 
   local signs = get_signs(vim.v.lnum)
 
-  local result = vim.v.virtnum == 0 and render_dap(signs[1]) or "  "
+  local left = "  "
+  if vim.v.virtnum == 0 then
+    left = signs[1] and render_dap(signs[1]) or render_diag(signs[2])
+  end
 
-  return result .. number_line() .. render_git(signs[2])
+  return left .. number_line() .. render_git(signs[3])
 end
 
 vim.o.statuscolumn = "%{%v:lua.require('pkg.statuscolumn').render()%}"
